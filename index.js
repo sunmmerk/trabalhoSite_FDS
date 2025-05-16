940db7128fb92afa495342ad259a720e06a1a5c6
Vinicius Pereira <venicioussantos@gmail.com>
2025-05-16 19:35:33 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('CliqueBotao').addEventListener('click', function() {
+    const msg = document.getElementById('mensagem');
+    msg.textContent = '"A felicidade nunca virá para aqueles que não apreciam o que já têm."';
+    msg.style.display = "block";
+});
